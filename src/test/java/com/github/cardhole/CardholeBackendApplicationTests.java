f30cd5749ef5f246c124df0e7758e3aa9419d49f
laxika <laxika91@gmail.com>
2023-07-20 13:35:37 +0200

    #1 - Initialized the project.

---
+++ src/test/java/com/github/cardhole/CardholeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cardhole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardholeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
